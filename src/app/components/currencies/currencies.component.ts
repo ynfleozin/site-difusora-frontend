0e1f0d74a497616505fc1ea02fdeb7c6a6bad0cf
ynfleozin <leonardoalvarengam@gmail.com>
2025-07-08 15:49:09 -0300

    add content and styles to currencies component

--- src/app/components/currencies/currencies.component.ts
+++ src/app/components/currencies/currencies.component.ts
@@ -8,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './currencies.component.scss'
 })
 export class CurrenciesComponent {
+  cotacoes = [
+    {
+      moeda: "Dolár",
+      preco: 5.4212,
+      icon: "assets/svgs/dolar.svg",
+      symbol: "USD"
+    },
+    {
+      moeda: "Euro",
+      preco: 6.3857,
+      icon: "assets/svgs/euro.svg",
+      symbol: "EUR"
+    },
+    {
+      moeda: "Bitcoin",
+      preco: 586.773,
+      icon: "assets/svgs/bitcoin.svg",
+      symbol: "BTC"
+    },
+    {
+      moeda: "Etherium",
+      preco: 13.667,
+      icon: "assets/svgs/etherium.svg",
+      symbol: "ETH"
+    },
 
+  ]
 }
