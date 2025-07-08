415961a3e8e3ca68b00669c01bdf71d4972ba60a
ynfleozin <leonardoalvarengam@gmail.com>
2025-07-08 15:14:28 -0300

    add currencies component

---
+++ src/app/components/currencies/currencies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currencies',
+  standalone: true,
+  imports: [],
+  templateUrl: './currencies.component.html',
+  styleUrl: './currencies.component.scss'
+})
+export class CurrenciesComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CurrenciesComponent } from "../../components/currencies/currencies.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CurrenciesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
