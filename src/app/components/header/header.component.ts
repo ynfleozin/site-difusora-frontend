c4f1713535b55847f2afd40083a0c1d38985c6ac
ynfleozin <leonardoalvarengam@gmail.com>
2025-07-14 23:58:46 -0300

    add floating radio mini player

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  
+  showPlayer = false;
+  isPlaying = false;
+
+  @ViewChild('audio') audioRef!: ElementRef<HTMLAudioElement>;
+
+  openPlayer() {
+    this.showPlayer = true;
+    setTimeout(() => this.audioRef?.nativeElement.play());
+    this.isPlaying = true;
+  }
+
+  togglePlay() {
+    const audio = this.audioRef.nativeElement;
+    if (this.isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    this.isPlaying = !this.isPlaying;
+  }
+
+  closePlayer() {
+    this.audioRef.nativeElement.pause();
+    this.showPlayer = false;
+    this.isPlaying = false;
+  }
 }
