3342e3026667eb6125df653efb269c14f51b8e96
ynfleozin <leonardoalvarengam@gmail.com>
2025-07-15 20:00:02 -0300

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewsComponent } from './pages/news/news.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeComponent,
     title: 'Difusora Colatina',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
-    path: "noticia/:slug",
+    path: 'noticia/:slug',
     component: NewsComponent,
-    title: 'Notícia - Difusora Colatina'
-  }
-
+    title: 'Notícia - Difusora Colatina',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Página não encontrada',
+  },
 ];
