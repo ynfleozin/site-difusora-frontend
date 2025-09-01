2f67da24fb73f3d728e4c5212a9017e490e7126d
ynfleozin <leonardoalvarengam@gmail.com>
2025-09-01 15:43:00 -0300

    add skeleton loader and empty state for latest news

--- src/app/components/latest-news/latest-news.component.ts
+++ src/app/components/latest-news/latest-news.component.ts
@@ -2,32 +2,38 @@ import { Component, inject, OnInit } from '@angular/core';
 import { NewsCardComponent } from '../news-card/news-card.component';
 import { NewsArticle } from '../../models/news-article.model';
 import { NewsService } from '../../services/news.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-latest-news',
   standalone: true,
-  imports: [NewsCardComponent],
+  imports: [NewsCardComponent, CommonModule],
   templateUrl: './latest-news.component.html',
-  styleUrl: './latest-news.component.scss',
+  styleUrls: ['./latest-news.component.scss'],
 })
 export class LatestNewsComponent implements OnInit {
   latestNews: NewsArticle[] = [];
+  isLoading = true;
 
   private newsService = inject(NewsService);
 
   ngOnInit(): void {
+    this.loadLatestNews();
+  }
+
+  private loadLatestNews(): void {
+    this.isLoading = true;
+
     this.newsService.getLatestNews().subscribe({
       next: (newsFromApi) => {
-        this.processNews(newsFromApi);
+        this.latestNews = newsFromApi.slice(0, 6);
+        this.isLoading = false;
       },
       error: (err) => {
         console.error('Erro ao buscar notícias:', err);
         this.latestNews = [];
+        this.isLoading = false;
       },
     });
-  }
-
-  private processNews(allNews: NewsArticle[]): void {
-    this.latestNews = allNews.slice(0, 6);
   }
 }
