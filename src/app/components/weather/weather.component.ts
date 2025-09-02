5b3120f14849692e6a703c9fe7582bc3d15c6d91
ynfleozin <leonardoalvarengam@gmail.com>
2025-09-02 15:43:35 -0300

    add dynamic day and night theme

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,4 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+// 1. Importe o CommonModule
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { Weather } from '../../models/weather.model';
 import { CapitalizePipe } from '../../pipes/capitalize.pipe';
@@ -6,16 +8,18 @@ import { CapitalizePipe } from '../../pipes/capitalize.pipe';
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [CapitalizePipe],
+  imports: [CommonModule, CapitalizePipe],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss',
 })
 export class WeatherComponent implements OnInit {
   weather!: Weather;
+  timeOfDayClass: string = '';
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.setDynamicBackground();
     this.weatherService.getWeather().subscribe({
       next: (apiData: Weather) => {
         this.weather = apiData;
@@ -24,5 +28,22 @@ export class WeatherComponent implements OnInit {
         console.error('Erro ao buscar dados do clima', err);
       },
     });
+  }
+
+  private setDynamicBackground(): void {
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'America/Sao_Paulo',
+      hour: 'numeric',
+      hour12: false,
+    };
+    const currentHour = parseInt(
+      new Intl.DateTimeFormat('pt-BR', options).format(new Date())
+    );
+
+    if (currentHour >= 6 && currentHour < 18) {
+      this.timeOfDayClass = 'day-gradient';
+    } else {
+      this.timeOfDayClass = 'night-gradient';
+    }
   }
 }
