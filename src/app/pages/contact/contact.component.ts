71813bf5b66b87c78a7da16fd833efedfe7845ab
ynfleozin <leonardoalvarengam@gmail.com>
2025-09-08 14:05:27 -0300

    structure contact page with mocked data and simulated submission

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, 
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  constructor() { }
+
+  public sendEmail(event: Event): void {
+    event.preventDefault();
+
+    console.log("Formulário enviado!");
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    
+    console.log('Dados a serem enviados:');
+    formData.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
+    
+    form.reset();
+  }
 }
