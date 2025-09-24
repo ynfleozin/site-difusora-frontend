761857f8e07676d667638d053a3b2d26bd8ba2df
ynfleozin <leonardoalvarengam@gmail.com>
2025-09-24 11:50:08 -0300

    fix

--- environments/environment-development.ts
+++ environments/environment-development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://site-difusora-backend.onrender.com/apii',
+  apiUrl: 'https://site-difusora-backend.onrender.com/api',
   emailJsServiceId: 'service_5jxfiqn',
   emailJsTemplateId: 'template_ypnmnsd',
   emailJsPublicKey: 'l0qEG_mt60L7sKJT1'
