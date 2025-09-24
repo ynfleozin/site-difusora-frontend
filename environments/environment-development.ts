8c1385e80e8603354fee7c7ab8bcf48bd988b97d
ynfleozin <leonardoalvarengam@gmail.com>
2025-09-24 11:49:53 -0300

    create empty container

--- environments/environment-development.ts
+++ environments/environment-development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://site-difusora-backend.onrender.com/api',
+  apiUrl: 'https://site-difusora-backend.onrender.com/apii',
   emailJsServiceId: 'service_5jxfiqn',
   emailJsTemplateId: 'template_ypnmnsd',
   emailJsPublicKey: 'l0qEG_mt60L7sKJT1'
